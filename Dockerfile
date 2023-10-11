399b3df1300e9b2bdd05f25d16caa69f9a94ba06
arthursantanna <memo04sc@gmail.com>
2023-10-11 08:54:05 -0300

    configuring docker

---
+++ Dockerfile
@@ -0,0 +1,51 @@
+# Dockerfile:  Gerador / construtor de imagens
+FROM python:3.11.6-alpine3.17
+LABEL mantainer="memo04sc@gmail.com"
+
+# Essa variável de ambiente é usada para controlar se o Python deve 
+# gravar arquivos de bytecode (.pyc) no disco. 1 = Não, 0 = Sim
+ENV PYTHONDONTWRITEBYTECODE 1
+
+# Define que a saída do Python será exibida imediatamente no console ou em 
+# outros dispositivos de saída, sem ser armazenada em buffer.
+# Em resumo, você verá os outputs do Python em tempo real.
+ENV PYTHONUNBUFFERED 1
+
+# Copia a pasta "blogapp" e "scripts" para dentro do container.
+COPY blogapp /blogapp
+COPY scripts /scripts
+
+# Entra na pasta djangoapp no container
+WORKDIR /blogapp
+
+# A porta 8000 estará disponível para conexões externas ao container
+# É a porta que vamos usar para o Django.
+EXPOSE 8000
+
+# RUN executa comandos em um shell dentro do container para construir a imagem. 
+# O resultado da execução do comando é armazenado no sistema de arquivos da 
+# imagem como uma nova camada.
+# Agrupar os comandos em um único RUN pode reduzir a quantidade de camadas da 
+# imagem e torná-la mais eficiente.
+RUN python -m venv /blogvenv && \
+    /blogvenv/bin/pip install --upgrade pip && \
+    /blogvenv/bin/pip install -r /blogapp/requirements.txt && \
+    adduser --disabled-password --no-create-home duser && \
+    mkdir -p /data/web/static && \
+    mkdir -p /data/web/media && \
+    chown -R duser:duser /blogvenv && \
+    chown -R duser:duser /data/web/static && \
+    chown -R duser:duser /data/web/media && \
+    chmod -R 755 /data/web/static && \
+    chmod -R 755 /data/web/media && \
+    chmod -R +x /scripts
+
+# Adiciona a pasta scripts e venv/bin 
+# no $PATH do container.
+ENV PATH="/scripts:/blogvenv/bin:$PATH"
+
+# Muda o usuário para duser
+USER duser
+
+# Executa o arquivo scripts/commands.sh
+CMD ["commands.sh"]
--- blogapp/project/settings.py
+++ blogapp/project/settings.py
@@ -11,21 +11,25 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*3#k%b((!tm@mof+(4tvi5+y=19%v78%y4b&tfn56#jerfhxj5'
+SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',') if h.strip()
+]
 
 
 # Application definition
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv("DB_ENGINE", 'change-me'),
+        'NAME': os.getenv("POSTGRES_DB", 'change-me'),
+        'USER': os.getenv("POSTGRES_USER", 'change-me'),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD", 'change-me'),
+        'HOST': os.getenv("POSTGRES_HOST", 'change-me'),
+        'PORT': os.getenv("POSTGRES_PORT", 'change-me'),
     }
 }
 
@@ -115,7 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# /data/web/static
+STATIC_ROOT = DATA_DIR / 'static'
+
+MEDIA_URL = '/media/'
+# /data/web/media
+MEDIA_ROOT = DATA_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
